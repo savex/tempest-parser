e676aa47aab84dd0230615ae4d72bed1e3eee217
Oleksandr Savatieiev <a.savex@gmail.com>
2018-10-19 11:29:32 +0200

    v2.47

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ entry_points = {
 
 setup(
     name="TempestParser",
-    version="0.2.46",
+    version="0.2.47",
     author="Alex Savatieiev",
     author_email="a.savex@gmail.com",
     classifiers=[
